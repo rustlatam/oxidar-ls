35462e9fbfa37a410abacbc8dcf776e1d907396d
Nicolas Antinori <nicolas_antinori@hotmail.com>
2024-10-25 19:51:52 -0300

    added stage4 tests

---
+++ tests/stage4.rs
@@ -0,0 +1,38 @@
+#![cfg(feature = "stage4")]
+
+mod helpers;
+
+use helpers::{split_ls_column_output, split_ls_output_by_newline};
+use rstest::rstest;
+use std::process::Command;
+
+const EXPECTED_HELP_TEXT: &str = r#"A tiny version of ls command
+
+Usage: oxidar-ls [OPTIONS] <PATH>
+
+Arguments:
+  <PATH>  Path to list
+
+Options:
+  -a, --all     Show hidden files
+  -F, --format  Format file names. Appendds a symbol at the end indicating the type of file. Slash ('/') is for directories and at sign ('@') is for symbolic links
+  -l, --list    Show the files in a list
+  -h, --human   Show the files in a list
+      --help
+"#;
+
+#[rstest]
+#[case::arguments_help(&["--help"])]
+fn test_stage_4(#[case] ls_arguments: &[&'static str]) {
+    let output = Command::new("./target/debug/oxidar-ls")
+        .args([ls_arguments, &["./test_dir"]].concat())
+        .output()
+        .expect("failed to execute process");
+
+    assert!(output.status.success());
+
+    assert_eq!(
+        EXPECTED_HELP_TEXT,
+        std::str::from_utf8(&output.stdout).unwrap()
+    );
+}
